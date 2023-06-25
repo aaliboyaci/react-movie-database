71a8fa254d9a85ca64d94a5ce277794c4b4859ca
aaliboyaci <aaliboyaci@gmail.com>
2023-06-26 02:33:19 +0300

    make some changes on favico and footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div><hr></hr>Footer</div>
+    <div><hr></hr><p><i>Movie DB App is designed and developed by Ali Boyaci / This App uses API from themoviedb.org</i></p></div>
   )
 }
