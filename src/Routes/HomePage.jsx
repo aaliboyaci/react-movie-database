9cb0aa5f0db4ae97daf9c4b5d5610d302dd5e6e2
aaliboyaci <aaliboyaci@gmail.com>
2023-06-26 04:53:01 +0300

    add random movie feature on homepage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,54 +1,63 @@
 import React from 'react';
 import './HomePage.css';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import cycle from "../assets/recycle.png"
+
 
 export default function HomePage() {
+
+  const [randomMovie, setRandomMovie] = useState(null);
+
+  const fetchRandomMovie = async () => {
+    try {
+      const randomPage = Math.floor(Math.random() * 50) + 1; // Rastgele bir sayfa numarası seçiyoruz (1-50 arası)
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=6ef10486c5df46ca61884c8b042d53bd&sort_by=popularity.desc&page=${randomPage}`
+      );
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.results.length); // Rastgele bir film indeksi seçiyoruz
+      const randomMovie = data.results[randomIndex];
+      setRandomMovie(randomMovie);
+    } catch (error) {
+      console.error('Error fetching random movie:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (!randomMovie) {
+      fetchRandomMovie();
+    }
+  }, []);
+
+  const handleRandomButtonClick = () => {
+    fetchRandomMovie();
+  };
+
+
+
+
   return (
     <div className="home-container">
       <div className="card">
         <div className="header">
-          <h2>Random Movies</h2>
-          <button >Random</button>
+          <h2>Random Movie Generator</h2>
+          <img src={cycle}
+            style={{ width: '30px' }} onClick={handleRandomButtonClick} />
         </div>
         <div className="content">
-          <img />
-          <p></p>
-          <img src="" alt="Random Movie" />
-          <p>Movie Title</p>
-          <img />
-          <p></p>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="header">
-          <h2>Random Actor</h2>
-          <button>Random</button>
-        </div>
-        <div className="content">
-          <img src="" alt="Random Actor" />
-          <p>Actor Name</p>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="header">
-          <h2>Search by Year</h2>
-          
-        </div>
-        <div className="content">
-          <input type="text" placeholder="Enter year " />
-          <button>Search</button>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="header">
-          <h2>Random TV Show</h2>
-          <button>Random</button>
-        </div>
-        <div className="content">
-          <img src="" alt="Random TV Show" />
-          <p>TV Show Title</p>
+          {randomMovie && (
+            <>
+              <Link to={`/Details/${randomMovie.id}`} className="movie-link">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${randomMovie.poster_path}`}
+                  alt="Random Movie"
+                />
+                <h3 className="movie-title">{randomMovie.title}</h3>
+                <p className="movie-year">{randomMovie.release_date.substring(0, 4)}</p>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
