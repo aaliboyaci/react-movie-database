a1b9dc333e1b2c371da551ab56322cf2166e0ebd
aaliboyaci <aaliboyaci@gmail.com>
2023-06-27 16:15:01 +0300

    make pagination and displaying changes on watchlst

--- src/Routes/WatchList.jsx
+++ src/Routes/WatchList.jsx
@@ -10,71 +10,59 @@ export default function WatchList() {
   const dispatch = useDispatch();
 
   const [page, setPage] = useState(1);
-  const moviesPerPage = 5;
+  const moviesPerPage = 20;
+  const totalPages = Math.ceil(favoriteMovies.length / moviesPerPage);
   const indexOfLastMovie = page * moviesPerPage;
   const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
   const currentMovies = favoriteMovies.slice(indexOfFirstMovie, indexOfLastMovie);
-
 
   const handleRemoveClick = (movieId) => {
     dispatch(removeFavoriteMovie(movieId));
   };
 
-
   return (
-    <div className="fav-container">
-      <h2>Watch List</h2>
-      {favoriteMovies.length === 0 ? (
-        <p>No movies in your watch list.</p>
-      ) : (
-        <ul>
-          {currentMovies.map((movie) => (
-
-
-            <li key={movie.id} className="fav-movie">
-              <Link to={`/Details/${movie.id}`} className="fav-link">
-                <div className="fav-left">
-                  <div className="fav-image">
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                  </div>
-                  <div className="fav-year">
-                    <h4>{movie.release_date}</h4>
-                  </div>
-                </div></Link>
-              <div className="fav-right">
+    <>
+      <h1>Your Watch List</h1>
+      <div className="fav-container">
+        {favoriteMovies.length === 0 ? (
+          <p>No movies in your watch list.</p>
+        ) : (
+          <>
+            {currentMovies.map((movie) => (
+              <div key={movie.id} className="fav-movie">
                 <Link to={`/Details/${movie.id}`} className="fav-link">
-
-                  <h3>{movie.title}</h3>
-                  <p>{movie.overview.slice(0, 150)}...</p></Link>
-                <button className="empty-button" onClick={() => handleRemoveClick(movie.id)}>Remove From Favs</button>
+                  <div className="fav-top">
+                    <div className="fav-image">
+                      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    </div>
+                    <div className="fav-year">
+                      <b>{movie.release_date.slice(0,4)}</b>
+                    </div>
+                  </div>
+                </Link>
+                <div className="fav-bot">
+                  <Link to={`/Details/${movie.id}`} className="fav-link">
+                    <div className="fav-title">{movie.title}</div>
+                    <div className="fav-des">{movie.overview.slice(0, 95)}...</div>
+                  </Link>
+                  <button className="empty-button" onClick={() => handleRemoveClick(movie.id)}>Remove From Favs</button>
+                </div>
               </div>
-            </li>
-
-
-          ))}
-        </ul>
-      )}
+            ))}
+          </>
+        )}
+      </div>
 
       {favoriteMovies.length > moviesPerPage && (
         <div className="pagination-buttons">
-          <button
-            disabled={page === 1}
-            onClick={() => setPage(page - 1)}
-          >
+          <button disabled={page === 1} onClick={() => setPage(page - 1)}>
             Previous
           </button>
-
-          <button
-            disabled={currentMovies.length < moviesPerPage}
-            onClick={() => setPage(page + 1)}
-          >
+          <button disabled={page === totalPages} onClick={() => setPage(page + 1)}>
             Next
           </button>
         </div>
       )}
-
-
-    </div>
-
+    </>
   );
 }
