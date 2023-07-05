0c138a4ffa4bd269f4c3ed3a10333453766ae3b2
aaliboyaci <aaliboyaci@gmail.com>
2023-07-05 20:44:42 +0300

    #16 refactor trendingpage

--- src/Routes/Trending.tsx
+++ src/Routes/Trending.tsx
@@ -1,42 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./SearchPage.css";
-import useFetch from "../Hooks/useFetch";
 import Loading from "../Components/Loading";
+import { Trend, trendingFetch } from '../Services/trendingFetch';
 
 
-interface Trend{
-  id: number;
-  title: string;
-  release_date: string;
-  poster_path: string;
-}
 const Trending: React.FC = () => {
   const [trendingMovies, setTrendingMovies] = useState<Trend[]>([]);
-  const apiKey = `6ef10486c5df46ca61884c8b042d53bd`;
-  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
-  const { isLoading, data, error } : any = useFetch(url);
-  
-  useEffect(() => {
-    if (!isLoading && data !== null && data.length !== 0) {
-      setTrendingMovies(data);
-    }
-  }, [isLoading, data]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  const { isLoading, error } = trendingFetch(setTrendingMovies)
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+  if (isLoading) { return <Loading />; }
+  if (error) { return <p>Error: {error}</p>; }
 
   return (
     <div id="search-container">
       <h2>Trending Movies</h2>
       {trendingMovies.length === 0 ? (
-        <p>No movies found.</p>
-      ) : (
+        <p>No movies found.</p>) : (
         <ul className="movie-list">
           {trendingMovies.map((movie) => (
             <li key={movie.id} className="movie-item">
