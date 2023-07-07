d6d5f69588e29d4bfdb6e3f397e47e6740b4d8e1
aaliboyaci <aaliboyaci@gmail.com>
2023-07-07 13:41:10 +0300

    #14 create services of personPage implement to app

--- src/Routes/PersonPage.tsx
+++ src/Routes/PersonPage.tsx
@@ -1,28 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import Loading from '../Components/Loading';
 import personFetch, { PersonDetails } from '../Services/personFetch';
 
 
+export interface popProps {
+    length: number;
+    map: any;
+    id: number;
+    title: string;
+
+}
 const PersonPage: React.FC = () => {
     const { personId } = useParams<{ personId: string }>();
     const [personDetails, setPersonDetails] = useState<PersonDetails | null>(null);
-    const isLoading = personFetch(setPersonDetails, personId);
+    const [popMovies, setPopMovies] = useState<popProps | null>(null);
+    const { isLoading1, isLoading2 } = personFetch(setPersonDetails, personId, setPopMovies);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+
+    const navigate = useNavigate();
+
     console.log(personDetails)
 
-    if (!personDetails) {
-        return <Loading />;
-    }
+    if (!personDetails) { return <Loading />; }
+    if (isLoading1 | isLoading2) { return <Loading />; }/*çift fetch olduğu için iki tane loading var*/
+
+    const handleGenreClick = (movieID: number) => {
+
+        navigate(`/Details/${movieID}`);
+    };
+    { console.log(popMovies) }
 
     return (
         <div>
             <h1>{personDetails.name}</h1>
             <img src={`https://image.tmdb.org/t/p/w500/${personDetails.profile_path}`} alt={personDetails.name} />
-            <p>Biography: {personDetails.biography}</p>
+            <p>Biography: {isExpanded ? personDetails.biography : `${personDetails.biography.slice(0, 200)}...`}<br></br>
+                <button onClick={() => setIsExpanded(!isExpanded)}>
+                    {isExpanded ? "Hide" : "Show more"}
+                </button></p>
             <p>Birthday: {personDetails.birthday}</p>
+            <p>Gender : {(personDetails.gender === 1) ? (<>Woman</>) : (<>Man</>)}</p>
             <p>Place of Birth: {personDetails.place_of_birth}</p>
             <h2>Popular Movies:</h2>
-           
+            {(popMovies == null || popMovies.length === 0) ? (
+                <p><Loading /></p>
+            ) : (
+                popMovies.map((movie: any) => (
+                    <div
+                        key={movie.id}
+                        onClick={() => handleGenreClick(movie.id)} >
+                        <div >Title movie: {movie.title}</div>
+
+                    </div>
+                ))
+            )}
+
         </div>
     );
 };
--- src/Services/personFetch.tsx
+++ src/Services/personFetch.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useFetch from "../Hooks/useFetch";
 import { baseUrl, apiKey } from '../Services/tmdbApiServices';
+import { popProps } from "../Routes/PersonPage"
 
 
 export interface PersonDetails {
@@ -12,35 +13,53 @@ export interface PersonDetails {
     biography: string;
     birthday: string
     place_of_birth: string;
-   
-  }
+    gender: number;
 
-const personFetch =  (setPersonDetails: React.Dispatch<React.SetStateAction<PersonDetails | null>>, personId: string |undefined) => {
+}
 
-    const { isLoading, data, error }: any = useFetch(`${baseUrl}person/${personId}?api_key=${apiKey}`);
-    const{isloa, data2, err2}: any = useFetch(`${baseUrl}person/${personId}/popular?api_key=${apiKey}`)
-    console.log("persondata2=" + data2) /*404 HATASI*/
-  
+const personFetch = (setPersonDetails: React.Dispatch<React.SetStateAction<PersonDetails | null>>, personId: string | undefined,
+    setPopMovies: React.Dispatch<React.SetStateAction<popProps | null>>) => {
+    const [job, setJob] = useState("");
+
+    const { isLoading: isLoading1, data: data1, error: error1 }: any = useFetch(`${baseUrl}person/${personId}?api_key=${apiKey}`);
+    const { isLoading: isLoading2, data: data2, error: error2 }: any = useFetch(`${baseUrl}person/${personId}/movie_credits?api_key=${apiKey}`)
+
+
+    /*kişi bilgisi için ilk fetch*/
     useEffect(() => {
-        if (!isLoading && data !== null && data.length !== 0) {
-            setPersonDetails(data);
-            console.log("persondata=" + data)
+        if (!isLoading1 && data1 !== null && data1.length !== 0) {
+            setPersonDetails(data1);
+            if (data1.known_for_department === "Acting") { setJob("Actor") }
+            if (data1.known_for_department === "Directing") { setJob("Director") }
+            console.log("bu kişi = " + job)
+
         }
-    }, [isLoading, data]);
+    }, [isLoading1, data1]);
 
-    
+    /*bulunduğu filmler için fetch*/
+    useEffect(() => {
+        if (!isLoading2 && data2 !== null && data2.length !== 0) {
+            if (job === "Actor") {
+                const filteredCast = data2.cast.filter((item: any) => !item.character.includes('(voice)'));
+                const sortedCast = filteredCast.sort((a: any, b: any) => b.popularity - a.popularity);
+                const top5Films = sortedCast.slice(0, 5);
+                setPopMovies(top5Films);
+            }
+            if (job === "Director") {
+                const filteredCast = data2.crew.filter((item: any) => item.department.includes("Directing"));
+                const sortedCast = filteredCast.sort((a: any, b: any) => b.popularity - a.popularity);
+                const top5Films = sortedCast.slice(0, 5);
+                setPopMovies(top5Films);
+            }
+        }
 
-    // useEffect(() => {
-    //     if (!isloa && data2 !== null && data2.length !== 0) {
-            
-    //         console.log("persondata2=" + data2)
-    //     }
-    // }, [isloa, data2]);
+    }, [isLoading2, data2]);
+    /* api responseları karışık olduğu için burada zorunlu olarak array işlemleri yapmam gerekti  */
 
-    
+
     return (
-        isLoading
-     )
+        { isLoading1, isLoading2 }
+    )
 };
 
 export default personFetch;
