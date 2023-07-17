16f883ee58230636136cdbe2253a0e70821bd398
aaliboyaci <aaliboyaci@gmail.com>
2023-07-17 16:32:30 +0300

    #29 fix Routing bug with absolute url

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.tsx'
+import { HashRouter as Router } from 'react-router-dom'; 
+import App from './App'
 import './index.css'
 
 const rootElement = document.getElementById('root')!;
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
+    <Router>
     <App />
-    
+    </Router>
   </React.StrictMode>,
 )
