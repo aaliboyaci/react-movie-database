2781570fba3dc0b73c5ed2c39590a88723f199a4
aaliboyaci <aaliboyaci@gmail.com>
2023-08-08 13:50:29 +0300

    #29

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './store';
