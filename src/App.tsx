36d815e92e751c30d786fdcefaf9269c5869ec02
aaliboyaci <aaliboyaci@gmail.com>
2023-08-15 22:22:38 +0300

    add app intro screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import store from "./store";
@@ -22,34 +23,53 @@ import {
   SEARCHPAGE,
   ERROR,
 } from "./presentation/Routes/routes";
+import Loader from "./presentation/Components/loaderIntro/Loader";
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowWelcome(false);
+    }, 2100);
+  }, []);
+
   return (
-    <div id="container">
-      <Provider store={store}>
-        <Router
-          basename={
-            process.env.NODE_ENV === "development"
-              ? "/"
-              : "/react-movie-database/"
-          }
-        >
-          <Header />
-          <Routes>
-            <Route path={HOME} element={<HomePage />} />
-            <Route path={TRENDING} element={<Trending />} />
-            <Route path={GENRESPAGE} element={<GenresPage />} />
-            <Route path={WATCHLIST} element={<WatchList />} />
-            <Route path={DETAILSPAGE} element={<MovieDetailsPage />} />
-            <Route path={PERSONPAGE} element={<PersonPage />} />
-            <Route path={SEARCHPAGE} element={<SearchPage />} />
-            <Route path={ERROR} element={<ErrorPage />} />
-          </Routes>
-          <br></br>
-          <Footer />
-        </Router>
-      </Provider>
-    </div>
+    <>
+      {showWelcome ? (
+        <div className="welcome">
+          <h2>Welcome to </h2>
+          <h1>Movie Database App</h1>
+          <Loader />
+        </div>
+      ) : (
+        <div id="container">
+          <Provider store={store}>
+            <Router
+              basename={
+                process.env.NODE_ENV === "development"
+                  ? "/"
+                  : "/react-movie-database/"
+              }
+            >
+              <Header />
+              <Routes>
+                <Route path={HOME} element={<HomePage />} />
+                <Route path={TRENDING} element={<Trending />} />
+                <Route path={GENRESPAGE} element={<GenresPage />} />
+                <Route path={WATCHLIST} element={<WatchList />} />
+                <Route path={DETAILSPAGE} element={<MovieDetailsPage />} />
+                <Route path={PERSONPAGE} element={<PersonPage />} />
+                <Route path={SEARCHPAGE} element={<SearchPage />} />
+                <Route path={ERROR} element={<ErrorPage />} />
+              </Routes>
+              <br></br>
+              <Footer />
+            </Router>
+          </Provider>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/presentation/Components/loaderIntro/Loader.tsx
@@ -0,0 +1,13 @@
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <>
+      <div className="loader">
+        <span className="loader__element"></span>
+        <span className="loader__element"></span>
+        <span className="loader__element"></span>
+      </div>
+    </>
+  );
+}
