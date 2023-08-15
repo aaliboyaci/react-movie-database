e38389ac4167e2b0849bfa609be4c36e62190436
aaliboyaci <aaliboyaci@gmail.com>
2023-08-15 23:09:49 +0300

    add header text to homepage

--- src/presentation/Routes/HomePage.tsx
+++ src/presentation/Routes/HomePage.tsx
@@ -5,6 +5,10 @@ import RandomMovie from "../Components/RandomMovie";
 const HomePage: React.FC = () => {
   return (
     <div className="home-container">
+      <div className="openingText">
+        Explore a vast collection of movies, from classic favorites to the
+        latest releases.
+      </div>
       <RandomMovie />
     </div>
   );
